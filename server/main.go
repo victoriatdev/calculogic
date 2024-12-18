a497cab3f7e82a7a54115b87b6c8a49e588391a4
sapphyree <sonexix@gmail.com>
2024-12-18 17:07:21 +0000

    Extract server instantiation.

--- server/main.go
+++ server/main.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 )
@@ -20,10 +19,16 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	hasSecond := r.URL.Query().Has("second")
 	second := r.URL.Query().Get("second")
 
-	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("could not read body: %s\n", err)
+	}
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s, body:\n%s\n",
 		ctx.Value(keyServerAddr),
 		hasFirst, first,
 		hasSecond, second,
+		body,
 	)
 	io.WriteString(w, "This is my website!\n")
 }
@@ -32,7 +37,29 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-	io.WriteString(w, "Hello, HTTP!\n")
+
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		w.Header().Set("x-missing-field", "myName")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
+}
+
+func constructServer(portNumber string, handler http.Handler, ctx context.Context) (*http.Server, context.CancelFunc) {
+	ctx, cancelCtx := context.WithCancel(ctx)
+	
+	constructedServer := &http.Server {
+		Addr: portNumber,
+		Handler: handler,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	return constructedServer, cancelCtx
 }
 
 func main() {
@@ -40,24 +67,13 @@ func main() {
 	multiplexer.HandleFunc("/", getRoot)
 	multiplexer.HandleFunc("/hello", getHello)
 
-	ctx, cancelCtx := context.WithCancel(context.Background())
-	serverOne := &http.Server{
-		Addr: ":3333",
-		Handler: multiplexer,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
-	}
+	ctx := context.Background()
+	
+	serverOne, cancelOne := constructServer(":3333", multiplexer, ctx)
+	defer cancelOne()
 
-	serverTwo := &http.Server{
-		Addr: ":4444",
-		Handler: multiplexer,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
-	}
+	serverTwo, cancelTwo := constructServer(":4444", multiplexer, ctx)
+	defer cancelTwo()
 
 	go func() {
 		err := serverOne.ListenAndServe()
@@ -66,7 +82,7 @@ func main() {
 		} else if err != nil {
 			fmt.Printf("error listening for server one: %s\n", err)
 		}	
-		cancelCtx()
+		cancelOne()
 	}()
 	go func() {
 		err := serverTwo.ListenAndServe()
@@ -75,7 +91,7 @@ func main() {
 		} else if err != nil {
 			fmt.Printf("error listening for server two: %s\n", err)
 		}	
-		cancelCtx()
+		cancelTwo()
 	}()
 
 	<-ctx.Done()

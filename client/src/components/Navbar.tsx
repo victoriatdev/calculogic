199a51a83e60af72f559d46b343c3a01aaedc05c
sapphyree <sonexix@gmail.com>
2025-01-20 18:41:40 +0000

    Update routing

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,6 +2,33 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const navbarLinks = [
+    {
+      name: "Home",
+      redirect: "/",
+    },
+    {
+      name: "Sequent Calculus",
+      redirect: "/sequent-calculus",
+    },
+    {
+      name: "Natural Deduction",
+      redirect: "/natural-deduction",
+    },
+    {
+      name: "Custom",
+      redirect: "/custom",
+    },
+    {
+      name: "About",
+      redirect: "/about",
+    },
+  ];
+
+  const navbarLinkItems = navbarLinks.map((link) => {
+    <Link to={link.redirect}>{link.name}</Link>;
+  });
+
   return (
     <header>
       <nav>
@@ -12,12 +39,7 @@ const Navbar = () => {
             </Link>
           </h2>
         </div>
-        <div className="internal-links">
-          <Link to="/about">About</Link>
-          <Link to="/about">About</Link>
-          <Link to="/about">About</Link>
-          <Link to="/about">About</Link>
-        </div>
+        <div className="internal-links">{navbarLinkItems}</div>
         <div className="social-links">
           <Link to="/about">About The Sites</Link>
         </div>

a693965c05962aab720dcd61af9cc61b6ba3287c
sapphyree <sonexix@gmail.com>
2025-02-23 21:55:49 +0000

    Add Docker support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 500,
+    }
+  }
 })

3316d3c6c50a4d873ceee4606e8168f31f77540c
sapphyree <sonexix@gmail.com>
2025-02-27 11:21:22 +0000

    Update backend with echo and update docker files

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module fyp.com/fyp-server
 go 1.23.5
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/echo/v4 v4.13.3 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- server/server.go
+++ server/server.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	
+//	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+//	setConfig()
+
+	fmt.Println("Starting server...")
+
 	e := echo.New()
+	e.HideBanner = true
+
+	//test_env := os.Getenv("TEST_SECRET")
+	//fmt.Println(test_env)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/users/:id", getUser)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func setConfig() {
+	envError := godotenv.Load(".env")
+
+	if envError != nil {
+		log.Fatal("Error loading .env file.")
+	}
 }
